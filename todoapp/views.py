e0631574cfe0e6b4da42555eafb61fb317027d1b
npwjim <npwjim@gmail.com>
2020-03-10 20:11:33 +0800

    Update views and add index.html in todoapp.

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
+def login(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/index/')
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return HttpResponseRedirect('/index/')
+    else:
+        return render(request, 'login.htm', locals())
