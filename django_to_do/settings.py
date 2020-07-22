0a30ff54ac917342cf5acbc8544074d86ce01add
npwjim <npwjim@gmail.com>
2020-07-23 00:33:52 +0800

    Code simplification

--- django_to_do/settings.py
+++ django_to_do/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,4 +133,4 @@ LOGIN_URL = '/users/login/'
 AUTH_USER_MODEL = 'users.CustomUser'
 
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
-from django.contrib import auth
-from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect, HttpResponse
-from django.template.defaultfilters import length
-from django.utils import timezone
 from django.urls import reverse
 
 from .models import Todo
@@ -71,22 +66,3 @@ def search_todo(request):
         })
     else:
         return HttpResponseRedirect(reverse("todoapp:list"))
-
-
-# class LoginView(auth_views.LoginView):
-#     template_name = 'todoapp/login.html'
-#
-#
-# class LogoutView(auth_views.LogoutView):
-#     next_page = '/todo/'
-#
-#
-# def register(request):
-#     if request.method == "POST":
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             return HttpResponseRedirect('/todo/accounts/login/')
-#     else:
-#         form = UserCreationForm(request.POST)
-#     return render(request, 'todoapp/register.html', locals())
